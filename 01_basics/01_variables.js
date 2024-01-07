4195ac8125a14c484db401bf6004bff1a4aeb5d7
Ravikant-Raj <143958122+Ravikant-Raj@users.noreply.github.com>
2024-01-07 14:51:15 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =124558 // const can't be change .
+let accountEmail = "15ravikantraj@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountstate;
+
+// accountId = 2// not allowed with const 
+accountEmail = "hdsdsdds@hf.com"
+accountPassword ="158745"
+accountCity = "Dehradun"
+/* 
+Prefer not to use  var 
+because of issue in block scope and functional scope 
+
+*/
+console.table([accountId , accountEmail, accountPassword , accountCity , accountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" ;// treat all js code as newer veersion 
+// alter(3+3) // we are using nodejs , not Browser 
+console.log(3+3);
+console.log("ravi");
+
+let name = "Ravi"
+let age = 22
+let isLoggedIn = false 
+
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//Boolean => true / false 
+// null => standalone Value
+// undefined => 
+// Symbol => unique
+
+// object 
+
+console.log(typeof age);
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object 
+
+
